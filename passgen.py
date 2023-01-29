2ca6038110b49681136f66220cb91692657a8ea0
DrPatroleum <droneairpatrol@protonmail.com>
2023-01-29 15:25:37 +0100

    lastest changes

--- passgen.py
+++ passgen.py
@@ -89,6 +89,7 @@ special_dict = {8: '8 hours',
                    18: '7qd years'}
 
 
+
 def click():
     if (check_numbers.get() == 1) and (check_letters.get() == 1) and (check_special_characters.get() == 1):
         generate_full()
@@ -104,9 +105,9 @@ def click():
         generate_letters_only()
     if (check_numbers.get() == 0) and (check_letters.get() == 0) and (check_special_characters.get() == 1):
         generate_spec_only()
-    label5 = Label(window,
-                   text="The password has been copied to clipboard!")
-    label5.pack()
+    #label5 = Label(window,
+    #               text="The password has been copied to clipboard!")
+    #label5.pack()
 
 
 def copy_option(password):
@@ -119,11 +120,14 @@ def create_result(password):
     copy_option(password)
 
 
-def check_passwd_power(self):
+def check_passwd_power(self, password):
     pwr_result = self[pass_len.get()]
-    power = 'Hacker need ' + pwr_result + 'to crack password by brute force'
+    power = 'Hacker need ' + pwr_result + ' to crack password by brute force'
     label4 = Label(window, text=power)
     label4.pack()
+    msgbox = 'Your password is:\n ' + str(password) + '\n\n' + str(
+        power) + '\n\n' + 'The password has been copied to clipboard!'
+    messagebox.showinfo('Generated password', msgbox)
 
 
 def generate_full():
@@ -131,52 +135,56 @@ def generate_full():
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(full_alphabet))
     create_result(password)
-    check_passwd_power(all_characters_dict)
-    
-    
-    messagebox.showinfo('Generated password', password)
+    check_passwd_power(all_characters_dict, password)
+
 
 def generate_letters_num():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(letters_num))
     create_result(password)
-    check_passwd_power(letters_num_dict)
+    check_passwd_power(letters_num_dict, password)
+
 
 def generate_num_spec():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(num_spec))
     create_result(password)
-    check_passwd_power(num_spec_dict)
+    check_passwd_power(num_spec_dict, password)
+
 
 def generate_letters_spec():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(letters_spec))
     create_result(password)
-    check_passwd_power(letters_spec_dict)
+    check_passwd_power(letters_spec_dict, password)
+
 
 def generate_numbers_only():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(digits))
     create_result(password)
-    check_passwd_power(numbers_dict)
+    check_passwd_power(numbers_dict, password)
+
 
 def generate_letters_only():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(letters))
     create_result(password)
-    check_passwd_power(letters_dict)
+    check_passwd_power(letters_dict, password)
+
 
 def generate_spec_only():
     password = ''
     for _ in range(pass_len.get()):
         password += ''.join(secrets.choice(special_chars))
     create_result(password)
-    check_passwd_power(special_dict)
+    check_passwd_power(special_dict, password)
+
 
 letters = string.ascii_letters
 digits = string.digits
@@ -191,8 +199,6 @@ window = Tk()
 window.title("PASSGEN")
 window.geometry("300x300")
 window.resizable(False, False)
-icon = PhotoImage(file='/Users/defcon/Documents/passgen/set.png')
-window.iconphoto(True, icon)
 
 label1 = Label(window,
                text="What the password should consist of?")
